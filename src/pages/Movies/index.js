cb45918fa3842bd9421a749a78bc1d54c7f5d33e
Denis Melnikov <066den@gmail.com>
2021-12-09 14:40:23 +0200

    first commit

--- src/pages/Movies/index.js
+++ src/pages/Movies/index.js
@@ -3,7 +3,7 @@ import Category from "./lib/Category";
 import Search from "./lib/Search";
 import Loading from "./../../lib/Loading";
 
-const API_KEY = process.env.REACT_APP_API_KEY
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 class Movies extends React.Component {
   state = {
@@ -12,20 +12,28 @@ class Movies extends React.Component {
   };
 
   componentDidMount() {
-    fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=terminator`)
+    fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=terminator`)
       .then((response) => response.json())
-      .then((data) => this.setState({ movies: data.Search, loading: false }));
+      .then((data) => this.setState({ movies: data.Search, loading: false }))
+      .catch((err) => {
+        console.error(err);
+        this.setState({ loading: false });
+      });
   }
 
   searchMovies = (str, type = "all") => {
     this.setState({ loading: true });
     fetch(
-      `http://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${
+      `https://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${
         type !== "all" ? `&type=${type}` : ""
       }`
     )
       .then((response) => response.json())
-      .then((data) => this.setState({ movies: data.Search, loading: false }));
+      .then((data) => this.setState({ movies: data.Search, loading: false }))
+      .catch((err) => {
+        console.error(err);
+        this.setState({ loading: false });
+      });
   };
 
   render() {
